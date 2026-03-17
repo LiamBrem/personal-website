49c9bcdc54cbd5849e237f13af1556082d598b66
Liam Brem <50599315+LiamBrem@users.noreply.github.com>
2026-03-17 19:24:54 +0000

    Updates

---
+++ assets/calculator-DdrmNzty.js
@@ -0,0 +1,26 @@
+const e=`---
+title: On Using A Calculator Before Learning How To Add 
+date: 2026-03-17
+description: How AI has harmed this generation's CS students
+---
+**TL;DR:** When going into CS, it doesn't make sense to use AI on your assignments. We don't see kindergarten teachers giving children calculators before learning basic addition. 
+
+Last semester, there was some notable drama in the CS department of my university when a professor failed a significant portion of their class for academic integrity violations. As it turned out, the professor had caught all of these students using AI to write some or all of a big programming assignment. 
+
+Following this, one of the biggest complaints I heard from students was that AI is used extensively in the industry (true), so professors should allow its use by students on assignments. 
+
+One student even wrote a Rate My Professor review saying:
+
+> "Every SWE uses AI to develop and write code. He doesn't think that is the case, he would've been a good prof 10 years ago but his philosophy is outdated in modern CS and he honor-coded and failed 60% for getting help with AI to write code. Midterms were fair though, and he is extremely knowledgeable in the subject."
+
+I honestly find this hilarious. You know what is used in the Engineering industry? *Calculators.* Did Engineering students learn everything that the calculator can do manually and by hand, first? *Yes.*
+
+Sure, AI is a great tool, but you're shooting yourself in the foot by using it on a basic, introductory CS assignment that you can't even do yourself. Just because it's getting better at writing code doesn't mean you can completely forego learning how to code. I mean, you're a CS student and you can't even *code*? 
+
+To give another analogy, imagine being a Physics PhD student working on some groundbreaking research. In order to do this, you need some complex math. Imagine writing a long theorem that utilizes formulas but *not knowing how one of these fundamental formulas even works.* Sure, you might understand how some of the pieces fit together at a high level. However, if you messed up somewhere, how do you ever think you're going to diagnose and find out where you went wrong if you don't even know how the pieces work?
+
+Now, I'm not saying that you shouldn't use AI to assist in software development. Honestly, it's likely becoming a crucial skill in this modern landscape. But, using it as a crutch to completely skip the fundamentals will lead to more harm down the road. Furthermore, once you begin using it more and more, your regular programming skills might get a little rusty. However, if I use a calculator every day instead of practicing mental math, I'll still be able to do the math *without* a calculator. 
+
+At the same time, if I'm using AI to assist programming, I should still be able to write the program without it, albeit maybe a bit slower. And this is what I think many students are lacking. After talking to a lot of people, I always hear that, without AI, they'll be able to look things up and figure it out. Honestly, though, this isn't the case and is just cope. As a TA, I've seen the skill level of many students, and it's concerning. 
+
+Coding isn't going away, especially as AI gets more expensive and things inevitably start breaking. While it may become cheaper and more convenient to produce, you can't expect to go anywhere without the fundamentals.  `;export{e as default};
